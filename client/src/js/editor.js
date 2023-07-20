9d06a1564d63070a1838ffd0e1c2dd53d52c4d76
bswiley <bswiley@gmail.com>
2023-07-20 00:46:03 -0500

    Verified 'js' folder files

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -2,10 +2,8 @@
 import { getDb, putDb } from './database';
 import { header } from './header';
 
-export default class {
+export default class Editor {
   constructor() {
-    const localData = localStorage.getItem('content');
-
     // check if CodeMirror is loaded
     if (typeof CodeMirror === 'undefined') {
       throw new Error('CodeMirror is not loaded');
@@ -22,21 +20,38 @@ export default class {
       tabSize: 2,
     });
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
-    getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
-    });
+    // Load the content of the editor from indexedDB or localStorage or use the header as a fallback
+    this.loadEditorContent();
 
     this.editor.on('change', () => {
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself is loses focus
+    // Save the content of the editor when the editor itself loses focus
     this.editor.on('blur', () => {
       console.log('The editor has lost focus');
       putDb(localStorage.getItem('content'));
     });
+  }
+
+  async loadEditorContent() {
+    try {
+      // Get the content from indexedDB
+      const data = await getDb();
+
+      if (data) {
+        console.info('Loaded data from IndexedDB, injecting into the editor');
+        this.editor.setValue(data);
+      } else {
+        // Fallback to localStorage if no data in indexedDB
+        const localData = localStorage.getItem('content');
+        this.editor.setValue(localData || header);
+      }
+    } catch (error) {
+      console.error('Error loading data from indexedDB:', error);
+      // Fallback to localStorage if any error occurs while loading data from indexedDB
+      const localData = localStorage.getItem('content');
+      this.editor.setValue(localData || header);
+    }
   }
 }
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -10,24 +10,32 @@ const loadSpinner = () => {
   const spinner = document.createElement('div');
   spinner.classList.add('spinner');
   spinner.innerHTML = `
-  <div class="loading-container">
-  <div class="loading-spinner" />
-  </div>
+    <div class="loading-container">
+      <div class="loading-spinner"></div>
+    </div>
   `;
   main.appendChild(spinner);
 };
 
 const editor = new Editor();
 
-if (typeof editor === 'undefined') {
+// Check if the editor is undefined or null to display the spinner
+if (!editor) {
   loadSpinner();
+} else {
+  // Assuming Editor class has a render method to render the editor UI
+  editor.render(main);
 }
 
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
-  // register workbox service worker
+  // Register the workbox service worker
   const workboxSW = new Workbox('/sw.js');
-  workboxSW.register();
+  try {
+    workboxSW.register();
+  } catch (error) {
+    console.error('Service worker registration failed:', error);
+  }
 } else {
   console.error('Service workers are not supported in this browser.');
 }
