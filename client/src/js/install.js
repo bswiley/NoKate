29ef14b77a86f72f6e8a0a746676c03727a44c86
bswiley <bswiley@gmail.com>
2023-08-09 00:45:26 -0500

    changed install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,32 +1,31 @@
-const butInstall = document.getElementById("buttonInstall");
+const butInstall = document.getElementById('buttonInstall');
 
 window.addEventListener('beforeinstallprompt', (event) => {
+  console.log("INSTALLING!", event)
+  window.deferredPrompt = event;
+  console.log("WINDOW", window.deferredPrompt)
+  butInstall.classList.toggle('hidden', false);
+});
 
-    // Store the triggered events
-    window.deferredPrompt = event;
-
-    // Remove the hidden class from the button.
-    butInstall.classList.toggle('hidden', false);
-  });
 
 butInstall.addEventListener('click', async () => {
-  
+  console.log('hello2')
   const promptEvent = window.deferredPrompt;
-
+  console.log("PROMPTENVEN>>>>",promptEvent)
   if (!promptEvent) {
    return;
   }
 
-  // Show prompt
   promptEvent.prompt();
   
-  // Reset the deferred prompt variable, it can only be used once.
   window.deferredPrompt = null;
   
   butInstall.classList.toggle('hidden', true);
+  console.log('goodbye')
 });
 
+
 window.addEventListener('appinstalled', (event) => {
-  // Clear prompt
-  window.deferredPrompt = null;
-}); 
+    window.deferredPrompt = null;
+  console.log('appinstalled', event);
+});
