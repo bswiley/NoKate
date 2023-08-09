4ad311aa38f7425d38aa603bb8816a833cdf252d
bswiley <bswiley@gmail.com>
2023-08-09 00:42:34 -0500

    changed database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,75 +1,40 @@
 import { openDB } from 'idb';
 
-const initdb = async () => {
-  try {
-    const db = await openDB('jate', 1, {
-      upgrade(db) {
-        if (!db.objectStoreNames.contains('jate')) {
-          db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-          console.log('jate database created');
-        }
-      },
-    });
-    console.log('jate database ready');
-    return db;
-  } catch (error) {
-    console.error('Error opening database:', error);
-    throw error;
-  }
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (id, content) => {
+  //console.error('Update database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id:id, jate:content });
+  const result = await request;
+  console.log('Data saved to the database', result);
+  return result;
 };
 
-export const putDb = async (content) => {
-  try {
-    console.log('Post to the database');
-
-    // Create a connection to the database and version we want to use.
-    const jateDb = await initdb();
-
-    // Create a new transaction and specify the database and data privileges.
-    const tx = jateDb.transaction('jate', 'readwrite');
-
-    // Open up the desired object store.
-    const store = tx.objectStore('jate');
-
-    // Use the .add() method on the store and pass in the content.
-    const request = store.put({ value: content });
-
-    // Get confirmation of the request.
-    const result = await request;
-    console.log('Data saved to the database', result);
-    return result;
-  } catch (error) {
-    console.error('Error putting data into the database:', error);
-    throw error;
-  }
-};
-
-// Export a function we will use to GET data from the database.
+// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  try {
-    console.log('GET from the database');
-
-    // Create a connection to the database and version we want to use.
-    const jateDb = await initdb();
-
-    // Create a new transaction and specify the database and data privileges.
-    const tx = jateDb.transaction('jate', 'readonly');
-
-    // Open up the desired object store.
-    const store = tx.objectStore('jate');
-
-    // Use the .get() method to get data with the specified ID (1 in this case).
-    const request = store.get(1);
-
-    // Get confirmation of the request.
-    const result = await request;
-    console.log('result.value', result);
-    return result;
-  } catch (error) {
-    console.error('Error getting data from the database:', error);
-    throw error;
-  }
+  //console.error('GET all from database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
 };
 
-// Call initdb to ensure the database is initialized when the module is imported.
+
 initdb();
