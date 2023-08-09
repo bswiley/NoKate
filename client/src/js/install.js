38ab498d4c9f6d1888855c777d8fec2ba875ce44
bswiley <bswiley@gmail.com>
2023-08-09 00:21:03 -0500

    changed webpack config js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,31 +1,32 @@
-const butInstall = document.getElementById('buttonInstall');
-let deferredPrompt;
+const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
-// Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent the default installation prompt
-  event.preventDefault();
-  // Save the event for later use
-  deferredPrompt = event;
-});
 
-// Implement a click event handler on the `butInstall` element
+    // Store the triggered events
+    window.deferredPrompt = event;
+
+    // Remove the hidden class from the button.
+    butInstall.classList.toggle('hidden', false);
+  });
+
 butInstall.addEventListener('click', async () => {
-  if (deferredPrompt) {
-    // Show the installation prompt
-    deferredPrompt.prompt();
+  
+  const promptEvent = window.deferredPrompt;
 
-    // Wait for the user to respond to the prompt
-    const choiceResult = await deferredPrompt.userChoice;
-
-    // Reset the deferredPrompt variable after the prompt is shown
-    deferredPrompt = null;
+  if (!promptEvent) {
+   return;
   }
+
+  // Show prompt
+  promptEvent.prompt();
+  
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);
 });
 
-// Add a handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-  // Perform any actions after the PWA is installed
-  console.log('App was installed.', event);
-});
+  // Clear prompt
+  window.deferredPrompt = null;
+}); 
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
@@ -11,7 +12,7 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
@@ -20,9 +21,11 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
+        title: 'No-Kate'
       }),
       new WebpackPwaManifest({
         fingerprints: false,
+        inject: true,
         name: 'NoKate',
         short_name: 'Jate',
         description: 'Not Kate text editor',
@@ -56,6 +59,7 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },
